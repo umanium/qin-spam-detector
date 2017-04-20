4967b366a6b70257a33d8d645e3335ec0b807a48
Luqman Abdul Mushawwir <abdulmushawwir@gmail.com>
2017-04-20 08:11:48 +0700

    adding Cleaner

--- SpamDetector/Services/Cleaner.cs
+++ SpamDetector/Services/Cleaner.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SpamDetector.Services
 {
     public class Cleaner
     {
+        public Cleaner() { }
+        public string stripNonAscii(string sentence)
+        {
+            string result = Regex.Replace(sentence, @"[^\u0000-\u007F]+", string.Empty);
+            return result;
+        }
+
+        public string normalizeAccentedChars(string sentence)
+        {
+            var newStringBuilder = new StringBuilder();
+            newStringBuilder.Append(sentence.Normalize(NormalizationForm.FormKD)
+                                            .Where(x => x < 128)
+                                            .ToArray());
+            return newStringBuilder.ToString();
+        }
     }
 }
--- SpamDetectorTest/CleanerTest.cs
+++ SpamDetectorTest/CleanerTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpamDetector.Services;
 
 namespace SpamDetectorTest
 {
@@ -7,8 +8,23 @@ namespace SpamDetectorTest
     public class CleanerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCleanerNormalizeAccentedWords()
         {
+            Cleaner cleaner = new Cleaner();
+            string accented = "das Mädchen küssen üben dünn die Prüfung für hálo";
+
+            string expected = "das Madchen kussen uben dunn die Prufung fur halo";
+            Assert.AreEqual(expected, cleaner.normalizeAccentedChars(accented));
+        }
+
+        [TestMethod]
+        public void TestCleanerStripNonAscii()
+        {
+            Cleaner cleaner = new Cleaner();
+            string nonAscii = "søme string";
+
+            string expected = "sme string";
+            Assert.AreEqual(expected, cleaner.stripNonAscii(nonAscii));
         }
     }
 }
