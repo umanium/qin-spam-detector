a67b307b59a713ea63bd584a08de428a8384585b
Luqman Abdul Mushawwir <abdulmushawwir@gmail.com>
2017-04-20 08:47:01 +0700

    add count vectorizer

---
+++ SpamDetector/Services/Vectorizer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpamDetector.Services
+{
+    public static class Vectorizer
+    {
+        public static Dictionary<string, int> countVectorizer(string[] tokens)
+        {
+            Dictionary<string, int> vectors = new Dictionary<string, int>();
+            for(int i = 0; i < tokens.Count(); i++)
+            {
+                if(vectors.ContainsKey(tokens[i]))
+                {
+                    vectors[tokens[i]] += 1;
+                }
+                else
+                {
+                    vectors.Add(tokens[i], 1);
+                }
+            }
+
+            return vectors;
+        }
+
+        public static Dictionary<string, int> countVectorizer(List<string[]> tokens)
+        {
+            List<string> flatTokens = new List<string>();
+            Dictionary<string, int> vectors = new Dictionary<string, int>();
+            for (int i = 0; i < tokens.Count(); i++)
+            {
+
+                flatTokens.AddRange(tokens[i]);
+            }
+            vectors = countVectorizer(flatTokens.ToArray());
+
+            return vectors;
+        }
+    }
+}
--- SpamDetectorTest/VectorizerTest.cs
+++ SpamDetectorTest/VectorizerTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using SpamDetector.Services;
 
 namespace SpamDetectorTest
 {
@@ -7,8 +9,40 @@ namespace SpamDetectorTest
     public class VectorizerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestVectorizeSingleArray()
         {
+            string[] tokens = new string[] { "she", "says", "that", "she", "heard", "you", "came"};
+
+            Dictionary<string, int> expected = new Dictionary<string, int>
+            {
+                { "she", 2 },
+                { "says", 1 },
+                { "that", 1 },
+                { "heard", 1 },
+                { "you", 1 },
+                { "came", 1 }
+            };
+            CollectionAssert.AreEqual(expected, Vectorizer.countVectorizer(tokens));
+        }
+
+        [TestMethod]
+        public void TestVectorizeListOfArrays()
+        {
+            List<string[]> tokens = new List<string[]>();
+            tokens.Add(new string[] { "she", "says", "that", "she", "heard", "you", "came" });
+            tokens.Add(new string[] { "she", "says", "that", "she", "heard", "you", "came" });
+            tokens.Add(new string[] { "she", "says", "that", "she", "heard", "you", "came" });
+
+            Dictionary<string, int> expected = new Dictionary<string, int>
+            {
+                { "she", 6 },
+                { "says", 3 },
+                { "that", 3 },
+                { "heard", 3 },
+                { "you", 3 },
+                { "came", 3 }
+            };
+            CollectionAssert.AreEqual(expected, Vectorizer.countVectorizer(tokens));
         }
     }
 }
